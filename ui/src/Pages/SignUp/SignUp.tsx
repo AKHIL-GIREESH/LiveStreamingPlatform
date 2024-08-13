50841780edc1ccee0af3843ab1a66372a42b632f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-08-13 08:17:44 +0530

    SignUp

--- ui/src/Pages/SignUp/SignUp.tsx
+++ ui/src/Pages/SignUp/SignUp.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react"
+
 const SignUp = () => {
+    
+    // const [SignUpData,setSignUpData] = useState({
+    //     username:"",
+    //     email:"",
+    //     password:""
+    // })
+    // const signUpFunc = (e) => {
+    //     const {name,value} = e.target
+    //     setSignUpData(prev => ({...prev,[name]:value}))
+    // }
+
+    // return(
+    // <div>
+    //     <h1>SIGNUP</h1>
+    //     <div>
+    //         <input className="border-2 border-black  m-1" type="text" name="username" value={SignUpData.username} onChange={(e) => updateCred(e)}/>
+    //         <input className="border-2 border-black  m-1" type="text" name="email" value={SignUpData.email} onChange={(e) => updateCred(e)}/>
+    //         <input className="border-2 border-black  m-1" type="text" name="password" value={SignUpData.password} onChange={(e) => updateCred(e)}/>
+    //         {/* <Button>Login</Button> */}
+    //         {error?<button className="bg-black p-1 text-white" onClick={signUpFunc}>{isPending?"Loading":"Login"}</button>:<button className="bg-black p-1 text-white" onClick={signUpFunc}>{isPending?"Loading":"Login"}</button>}
+    //     </div>
+    // </div>)
     return(
-    <div>
-        SignUp
-    </div>)
+        <h1>SIGNUP</h1>
+    )
 }
 
 export default SignUp
