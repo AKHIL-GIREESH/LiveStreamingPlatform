4c5f099d746c13bacf54033575240a768b58e66c
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-09-15 08:07:57 +0530

    Chat fixed

--- ui/src/Pages/Dashboard/Chat/Chat.tsx
+++ ui/src/Pages/Dashboard/Chat/Chat.tsx
@@ -1,7 +1,7 @@
 import { updateStream } from "@/API/updateStream"
 import { useUser } from "@/AuthProvider"
 import { Switch } from "@/components/ui/switch"
-import { useGetStream } from "@/StreamContext"
+import { useGetStream, useSetStream } from "@/StreamContext"
 import { Stream, StreamChat } from "@/types/Stream"
 import { useMutation } from "@tanstack/react-query"
 
@@ -9,6 +9,7 @@ const Chat = () => {
 
     const stream = useGetStream()
     const user = useUser()
+    const setStream = useSetStream()
     console.log(stream)
 
     if((stream  === null) || (user === null)){
@@ -20,19 +21,21 @@ const Chat = () => {
     const {mutate,isPending,error} = useMutation({
         mutationFn: async (newStreamData : StreamChat) => { 
             const newStream = await updateStream(user._id,newStreamData)
-
+            setStream(newStream)
         }
     })
 
     const updateStreamHandler = (n : keyof Stream) => {
         console.log({[n]:stream[n]?false:true})
-
         mutate({[n]:stream[n]?false:true})
+    }
 
+    if(isPending){
+        return(<>Updating...</>)
+    }
 
-        
-        
-        
+    if(error){
+        return(<>Something went wrong {error}</>)
     }
 
     return(
